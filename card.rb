aa44fbc46d63cf1a3fdf258eac4fe8ef4aadf320
chris Zingel <chris@zingel.co.nz>
2012-11-21 23:39:02 +1300

    Add logic for AMEX to pass the two test (positive and negative)

--- card.rb
+++ card.rb
@@ -1,10 +1,21 @@
 class Card
   def initialize(card_number)
-    @card_number = card_number 
+    @card_number = card_number.to_s.gsub(/\D/,"")
+  end
+
+  def begins_with(number_of_digits)
+    @card_number[0..number_of_digits]
+  end
+
+  def length_being?(number_of_digits)
+    @card_number.length == number_of_digits
   end
 
   def card_type
-    "AMEX"
+    case 
+    when ["34","37"].include?(begins_with(1)) && length_being?(15)
+      return   "AMEX"
+    end
   end
 end
 
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -3,9 +3,14 @@ require "#{File.dirname(__FILE__)}/spec_helper"
 describe Card do
   before do
     @valid_card = "341234567890123"
+    @in_valid_card ="invalid_account_number"
   end
 
-  it "should classify AMEX cards" do
+  it "for a valid AMEX card it should classify it as a AMEX cards" do
     Card.new(@valid_card).card_type.should eq("AMEX")
+  end
+
+  it "for a invalid AMEX card it should not classify it as a AMEX cards" do
+    Card.new(@in_valid_card).card_type.should_not eq("AMEX")
   end
 end
